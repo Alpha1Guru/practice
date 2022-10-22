e13deb2c667eabbd6c09797d0f64602299016ba6
Pius <109897479+Alpha1Guru@users.noreply.github.com>
2022-10-22 07:25:06 +0100

    bug fixes

--- fun/Python-Practice/Mathematics/binary.py
+++ fun/Python-Practice/Mathematics/binary.py
@@ -5,12 +5,15 @@ def binary():
     print("Type y to convert a base ten number and q to quit");
     while True:
         res=input("Your response: ");
+        
+        while (res != "y" and res != 'q'):
+            print("Wrong input TRY AGAIN");
+            res=input("Your response: ");
+            
         if res=='q':
             print('\nBye');
             break
-        while (res != "y" and res != 'p'):
-            print("Wrong input TRY AGAIN");
-            res=input("Your response: ");
+
         if res =="y":
             ans=[];
             base_ten=int(input("Enter base_ten number: "));
@@ -26,7 +29,7 @@ def binary():
             while count!=0:
                 count-=1;
                 print("\t",ans[count]);
-        print("Want to convert more numbers?");
+            print("Want to convert more numbers?");
 
 
 binary()
